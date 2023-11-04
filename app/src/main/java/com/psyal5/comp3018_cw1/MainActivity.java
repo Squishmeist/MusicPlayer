c3bb0bbe237f6d619d5f6c3eccee30d389f82f23
Ainsley Lee <psyal5@exmail.nottingham.ac.uk>
2023-11-04 14:07:16 +0000

    initial commit: base android java project

---
+++ app/src/main/java/com/psyal5/comp3018_cw1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.psyal5.comp3018_cw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
